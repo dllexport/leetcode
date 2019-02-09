5fe60caa7df6fef914e6bf2d2aefa8a9337c501a
Mario Lau <liuminghao233@gmail.com>
2019-02-09 21:44:34 +0800

    ..

---
+++ 942. DI String Match/main.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<int> diStringMatch(string S) {
+
+    auto size = S.size();
+
+    vector<int> vec(size + 1, 0);
+
+    int min = 0;
+    int max = S.size();
+
+
+    for (int j = 0; j < size; ++j)
+    {
+        switch (S[j])
+        {
+            case 'I':
+            {
+                vec[j] = min++;
+                break;
+            }
+            case 'D':
+            {
+                vec[j] = max--;
+                break;
+            }
+        }
+
+    }
+    vec.back() = max;
+    return vec;
+
+}
+
+int main()
+{
+    auto i = diStringMatch("IDID");
+
+    return 0;
+}
