084dd4fc160ebed2754c2df376f89ae141e3d901
Mario Lau <liuminghao233@gmail.com>
2019-06-03 17:31:07 +0800

    ..

---
+++ 509. Fibonacci Number/main.go
@@ -0,0 +1,16 @@
+package main
+
+func fib(N int) int {
+	if N == 0 {
+		return 0
+	}
+	if N == 1 {
+		return 1
+	}
+
+	return fib(N-1) + fib(N-2)
+}
+
+func main() {
+	println(fib(3))
+}
---
+++ 944. Delete Columns to Make Sorted/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func minDeletionSize(A []string) int {
+
+	if A == nil {
+		return 0
+	}
+	var arrSize = len(A)
+	if arrSize == 0 {
+		return 0
+	}
+	var elemSize = len(A[0])
+	var res int
+	for i := 0; i < elemSize; i++ {
+		for j := 1; j < arrSize; j++ {
+			fmt.Printf("%c < %c ?\n", A[j][i], A[j-1][i])
+			if A[j][i] < A[j-1][i] {
+				res++
+				break
+			}
+		}
+	}
+	return res
+}
+
+func main() {
+	var test [3]string
+	test[0] = "zyx"
+	test[1] = "wvu"
+	test[2] = "tsr"
+	res := minDeletionSize(test[:])
+	println(res)
+}
