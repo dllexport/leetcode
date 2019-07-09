709676e79f230dc8a1f635906dc96acec7b06b87
Mario Lau <liuminghao233@gmail.com>
2019-07-09 15:17:41 +0800

    ..

---
+++ 985. Sum of Even Numbers After Queries/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func sumEvenAfterQueries(A []int, queries [][]int) []int {
+	Answer := make([]int, len(A))
+	for i := 0; i < len(A); i++ {
+		val := queries[i][0]
+		index := queries[i][1]
+		fmt.Printf("val: %d, index: %d", val, index)
+
+		A[index] += val
+		for _, v := range A {
+			if v%2 == 0 {
+				Answer[i] += v
+			}
+		}
+	}
+	return Answer
+}
+
+func main() {
+	A := []int{1, 2, 3, 4}
+	Q := [][]int{{1, 0}, {-3, 1}, {-4, 0}, {2, 3}}
+	sumEvenAfterQueries(A, Q)
+}
