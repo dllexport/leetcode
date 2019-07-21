e5bd740b2c270503f0ed492be99f0cfac6302435
Mario Lau <mariolau@Marios-iMac.local>
2019-07-21 21:59:32 +0800

    ..

---
+++ 237. Delete Node in a Linked List/main.go
@@ -0,0 +1,14 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteNode(node *ListNode) {
+	node.Val = node.Next.Val
+	node.Next = node.Next.Next
+}
+func main() {
+
+}
